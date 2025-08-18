05aac54a6b5c6b5d39c79d53c7c3a6725756d4b0
Jaccobs1 <jonasddeus1@gmail.com>
2025-08-18 19:38:50 -0300

    Classe Adicao adicionada com o método somar

---
+++ Calculadora/Calculadora/Operações/Adicao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Operações
+{
+    internal class Adicao
+    {
+        public double Somar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
