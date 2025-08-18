fd34d20ef2f93c21c59b62f5703191c12b10588b
tassianewanderweger <wanderweger@gmail.com>
2025-08-18 19:52:12 -0300

    Incluída classe Divisão com método Dividir

---
+++ Calculadora/Calculadora/Operações/Divisao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Operações
+{
+    internal class Divisao
+    {
+        public double Dividir(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
