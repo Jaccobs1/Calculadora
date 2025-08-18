864237fbbb2d13bbfb01956916e6c86cdae53e04
Jaccobs1 <jonasddeus1@gmail.com>
2025-08-18 19:55:21 -0300

    Criando corpo do programa

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -1,1 +1,36 @@
-﻿// CALCULADORA
+﻿using Calculadora.Utils;
+
+string opcao = "";
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("CALCULADORA\n");
+
+    Console.WriteLine("1 - Adição");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Multiplicação");
+    Console.WriteLine("4 - Divisão");
+    Console.WriteLine("0 - SAIR\n");
+
+    Console.Write("Opção: ");
+    opcao = Console.ReadLine();
+
+    if (opcao != "0" && opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4")
+    {
+        ConsoleUtils.MensagemErro();
+    }
+
+} while (opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4");
+
+switch (opcao)
+{
+    case "1":
+        break;
+    case "2":
+        break;
+    case "3":
+        break;
+    case "4":
+        break;
+}
